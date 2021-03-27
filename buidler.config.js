2c19817ce5e6b19ebde2fe25d66ffa3c766e864c
Cameron Joyce <61301130+CameronBJoyce@users.noreply.github.com>
2021-03-27 08:58:13 -0400

    Add files via upload

---
+++ buidler.config.js
@@ -0,0 +1,11 @@
+import { BuidlerConfig } from "@nomiclabs/buidler/config";
+
+const config: BuidlerConfig = {
+    solc:{
+        version:"0.5.12"
+    }
+};
+
+export default config;
+
+module.exports = {};
